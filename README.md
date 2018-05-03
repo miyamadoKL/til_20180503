820fd4a673b59fd035f704178aa4b254ec2cf1cb
miyamadoKL <105kansai500@gmail.com>
2018-05-03 19:54:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# What I learned on May 3, 2018
+Calling a function recursively to flatten multi-dimensional arrays.
---
+++ powershell/recursion.ps1
@@ -0,0 +1,34 @@
+$a = @(1, @(2, @(3, @(4, @(5)))))
+$b = @(1, 2, 3, 4, 5)
+
+function f($n) {
+    $ret = @()
+    foreach($x in $n) {
+        if (!($x -is [array])) {
+            $ret = @($x)
+        } else {
+            $ret += f($x)
+        }
+    }
+    return $ret
+}
+
+$c = f($a)
+
+$d = ""
+
+#Check the number of elements are equal.
+Compare-Object $b $c -PassThru | ForEach-Object{$d += $_}
+
+#Check the order of elements are equal.
+for ($i=0;$i -lt [Math]::Min($b.length, $c.Length);$i++) {
+    if ($b[$i] -ne $c[$i]) {
+        $d += $b[$i]
+    }
+}
+
+if ($d.Length -eq 0){    
+    Write-Host "OK!"
+} else {
+    Write-Host "NG"
+}
